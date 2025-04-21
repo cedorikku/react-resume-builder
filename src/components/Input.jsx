0065ec25855542acd22299a5fc6cb7c1922552bb
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-04-21 21:43:32 +0800

    Add animation to Inputs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,18 @@
-const Input = ({ type = 'text', label, onChange }) => {
+import { v4 as uuidv4 } from 'uuid';
+
+const Input = ({ type = 'text', label, placeholder, onChange }) => {
+    const id = uuidv4();
     return (
-        <div className="input-group flex flex-col gap-1">
-            <label className="text-sm font-medium" htmlFor={name}>
-                {label || name}
+        <div className="input-group flex flex-col gap-1 w-[100%]">
+            <label className="text-sm font-medium" htmlFor={id}>
+                {label || ''}
             </label>
             <input
-                className="bg-background-300 mb-4 rounded-sm border-1 border-white/25 p-4"
+                id={id}
+                className="bg-background-200 focus:bg-background-300 mb-4 rounded-sm border-1 border-white/25 p-4 transition-colors hover:border-white/75"
                 type={type}
-                name={name}
                 onChange={onChange}
-                placeholder={label}
+                placeholder={label || placeholder}
             ></input>
         </div>
     );
