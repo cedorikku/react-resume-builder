074293d6942724f3fbc3c7013f3ea9445ffe4993
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-04-25 20:21:58 +0800

    Stretch buttons to full-width

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ export const Button = ({ type = 'button', text, handleClick }) => (
     <button
         type={type}
         onClick={handleClick}
-        className="hover:bg-primary-300 focus:bg-primary-300 bg-primary-400 mb-4 cursor-pointer rounded-sm px-8 py-4 font-semibold text-white active:scale-95"
+        className="hover:bg-primary-300 focus:bg-primary-300 bg-primary-400 mb-4 w-[100%] cursor-pointer rounded-sm px-8 py-4 font-semibold text-white active:scale-95"
     >
         {text}
     </button>
