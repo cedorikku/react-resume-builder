4c189aa935cf30e5270360396cb95c6d66c13038
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-04-25 20:56:48 +0800

    Remove input bottom padding

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,13 +3,13 @@ import { v4 as uuidv4 } from 'uuid';
 const Input = ({ type = 'text', label, placeholder, onChange }) => {
     const id = uuidv4();
     return (
-        <div className="input-group flex flex-col gap-1 w-[100%]">
+        <div className="input-group flex w-[100%] flex-col gap-1">
             <label className="text-sm font-medium" htmlFor={id}>
                 {label || ''}
             </label>
             <input
                 id={id}
-                className="bg-background-200 focus:bg-background-300 mb-4 rounded-sm border-1 border-white/25 p-4 transition-colors hover:border-white/75"
+                className="bg-background-200 focus:bg-background-300 rounded-sm border-1 border-white/25 p-4 transition-colors hover:border-white/75"
                 type={type}
                 onChange={onChange}
                 placeholder={label || placeholder}
