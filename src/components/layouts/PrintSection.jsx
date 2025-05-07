f8435c33c0d7aa59c4087c6fc8633551e84e2794
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-05-07 21:30:27 +0800

    Update print section headers text style

--- src/components/layouts/PrintSection.jsx
+++ src/components/layouts/PrintSection.jsx
@@ -1,9 +1,11 @@
 export const PrintSection = ({ name, children }) => {
     return (
         <section id={name.toLowerCase() + '-section'} className="mb-4">
-            <h2>{name}</h2>
+            <h2>{name.toUpperCase()}</h2>
             <hr />
-            {children}
+            <div className="px-4">
+                {children}
+            </div>
         </section>
     );
 };
