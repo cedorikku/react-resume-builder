89d38631cf8d796c24534f7160d5bbc94a0c7fab
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-05-07 18:05:30 +0800

    Adjust input label position and spacing

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,13 +3,20 @@ import { v4 as uuidv4 } from 'uuid';
 const Input = ({ type = 'text', label, placeholder, onChange }) => {
     const id = uuidv4();
     return (
-        <div className="flex w-[100%] flex-col gap-1">
-            <label className="text-sm font-medium" htmlFor={id}>
-                {label || ''}
-            </label>
+        <div className="flex w-[100%] items-center">
+            {label ? (
+                <label
+                    className="min-w-[15ch] text-sm font-medium"
+                    htmlFor={id}
+                >
+                    {label || ''}
+                </label>
+            ) : (
+                ''
+            )}
             <input
                 id={id}
-                className="bg-background-200 focus:bg-background-300 rounded-sm border-1 border-white/25 p-4 transition-colors hover:border-white/75"
+                className="bg-background-200 focus:bg-background-300 w-[100%] rounded-sm border-1 border-white/25 p-4 transition-colors hover:border-white/75"
                 type={type}
                 onChange={onChange}
                 placeholder={label || placeholder}
