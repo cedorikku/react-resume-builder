94dbeddddaf133a19c5c338f86f41287503e01b4
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-07-27 22:19:49 +0800

    Add anchor to each form title

--- src/components/layouts/FormSection.jsx
+++ src/components/layouts/FormSection.jsx
@@ -4,7 +4,14 @@ export const FormSection = ({ name, children }) => {
             id={name.toLowerCase() + '-form'}
             className="mb-8 rounded-lg border border-white/30 bg-zinc-900 p-12"
         >
-            <h2 className="flex justify-center font-bold">{name}</h2>
+            <h2 className="flex justify-center font-bold">
+                <a
+                    href={`#${name.toLowerCase()}-form`}
+                    className="hover:text-white/80"
+                >
+                    {name}
+                </a>
+            </h2>
             {children}
         </section>
     );
