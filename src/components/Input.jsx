9410f808fd4d184fa62861d417f9a646ecac8b15
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-08-01 21:03:42 +0800

    fix: Input component not reflecting state value

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-const Input = ({ type = 'text', label, placeholder, onChange }) => {
+const Input = ({ type = 'text', value = '', label, placeholder, onChange }) => {
     const id = uuidv4();
     return (
         <div className="flex w-[100%] flex-col">
@@ -18,6 +18,7 @@ const Input = ({ type = 'text', label, placeholder, onChange }) => {
                 id={id}
                 className="bg-background-200 focus:bg-background-300 w-[100%] rounded-sm border-1 border-white/25 p-4 transition-colors hover:border-white/75"
                 type={type}
+                value={value}
                 onChange={onChange}
                 placeholder={label || placeholder}
             ></input>
