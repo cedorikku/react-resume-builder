32b3150166f8e65bbdeacfdfc1457ea684123c11
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-08-02 21:04:36 +0800

    Add form visibility toggle, think Accordions

--- src/components/layouts/FormSection.jsx
+++ src/components/layouts/FormSection.jsx
@@ -1,18 +1,34 @@
+import { useState } from 'react';
+import Button from '../Button';
+
 export const FormSection = ({ name, children }) => {
+    const [isVisible, setIsVisible] = useState(false);
+
     return (
         <section
             id={name.toLowerCase() + '-form'}
-            className="mb-8 rounded-lg border border-white/30 bg-zinc-900 p-12"
+            className="mb-8 rounded-lg border border-white/30 bg-zinc-900 p-12 w-350"
         >
-            <h2 className="flex justify-center font-bold">
-                <a
-                    href={`#${name.toLowerCase()}-form`}
-                    className="hover:text-white/80"
-                >
-                    {name}
-                </a>
-            </h2>
-            {children}
+            <div className="flex justify-between mb-6">
+                <h2 className="font-bold">
+                    <a
+                        href={`#${name.toLowerCase()}-form`}
+                        className="hover:text-white/80"
+                    >
+                        {name}
+                    </a>
+                </h2>
+
+                <div className="flex basis-20">
+                    <Button
+                        text={isVisible ? "⯅" : "⯆"}
+                        handleClick={() => {
+                            setIsVisible(!isVisible);
+                        }}
+                    />
+                </div>
+            </div>
+            {isVisible && children}
         </section>
     );
 };
