8f48e2b895769fc5437c666a65a8789866cee6ce
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-08-03 12:21:52 +0800

    Fix problem with building class names dynamically
    Problem was introduced in fd9d3c6

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,12 +4,12 @@ export const Button = ({
     color = 'white',
     handleClick,
 }) => {
-    const bgColor = color !== 'white' ? `bg-${color}-500` : `bg-${color}`;
-    const textColor = color !== 'white' ? 'text-white' : 'text-black';
-    const hoverColor =
-        color !== 'white' ? `hover:bg-${color}-600` : 'hover:bg-white/80';
+    const colorVariants = {
+        red: 'bg-red-600 hover:bg-red-500 text-white',
+        white: 'bg-white hover:bg-white/80 text-black',
+    };
 
-    const classes = `w-full cursor-pointer ${textColor} rounded-sm ${bgColor} px-8 py-4 font-bold transition-colors ${hoverColor} active:scale-95`;
+    const classes = `w-full cursor-pointer rounded-sm ${colorVariants[color]} px-8 py-4 font-bold transition-colors active:scale-95`;
 
     return (
         <button type={type} onClick={handleClick} className={classes}>

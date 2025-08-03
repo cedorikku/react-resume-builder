cedorikku/react-resume-builder

commit fd9d3c6a2b6075fa687d9111fe6136177cfdc977

Update button component to be dynamically colored

src/components/Button.jsx
@@ -1,21 +1,21 @@
-export const Button = ({ type = 'button', text, handleClick }) => (
-    <button
-        type={type}
-        onClick={handleClick}
-        className="w-[100%] cursor-pointer rounded-sm bg-white px-8 py-4 font-bold text-black transition-colors hover:bg-white/80 active:scale-95"
-    >
-        {text}
-    </button>
-);
+export const Button = ({
+    type = 'button',
+    text,
+    color = 'white',
+    handleClick,
+}) => {
+    const bgColor = color !== 'white' ? `bg-${color}-500` : `bg-${color}`;
+    const textColor = color !== 'white' ? 'text-white' : 'text-black';
+    const hoverColor =
+        color !== 'white' ? `hover:bg-${color}-600` : 'hover:bg-white/80';
 
-export const ButtonRed = ({ type = 'button', text, handleClick }) => (
-    <button
-        type={type}
-        onClick={handleClick}
-        className="w-[100%] cursor-pointer rounded-sm bg-red-500 px-8 py-4 font-bold text-white transition-colors hover:bg-red-600 active:scale-95"
-    >
-        {text}
-    </button>
-);
+    const classes = `w-full cursor-pointer ${textColor} rounded-sm ${bgColor} px-8 py-4 font-bold transition-colors ${hoverColor} active:scale-95`;
+
+    return (
+        <button type={type} onClick={handleClick} className={classes}>
+            {text}
+        </button>
+    );
+};
 
 export default Button;

src/Form.jsx
@@ -1,7 +1,7 @@
 import './styles/Form.css';
 import { useContext } from 'react';
 import Input from './components/Input';
-import Button, { ButtonRed } from './components/Button';
+import Button from './components/Button';
 import { FormSection } from './components/layouts/FormSection';
 import { ProfileContext } from './contexts/profile-context';
 import { EducationContext } from './contexts/education-context';
@@ -133,8 +133,9 @@ const Form = () => {
                                                         }
                                                     />
                                                     <div className="flex basis-20">
-                                                        <ButtonRed
+                                                        <Button
                                                             text="-"
+                                                            color="red"
                                                             handleClick={() =>
                                                                 experienceHandlers.removeResponsibility(
                                                                     expItem.key,
@@ -151,8 +152,9 @@ const Form = () => {
                             ) : (
                                 ''
                             )}
-                            <ButtonRed
+                            <Button
                                 text="Remove"
+                                color="red"
                                 handleClick={() =>
                                     experienceHandlers.remove(expItem.key)
                                 }
@@ -229,8 +231,9 @@ const Form = () => {
                                                         }
                                                     />
                                                     <div className="flex basis-20">
-                                                        <ButtonRed
+                                                        <Button
                                                             text="-"
+                                                            color="red"
                                                             handleClick={() =>
                                                                 projectHandlers.removeDescription(
                                                                     projectItem.key,
@@ -248,8 +251,9 @@ const Form = () => {
                                 ''
                             )}
 
-                            <ButtonRed
+                            <Button
                                 text="Remove"
+                                color="red"
                                 handleClick={() =>
                                     projectHandlers.remove(projectItem.key)
                                 }
@@ -324,8 +328,9 @@ const Form = () => {
                                 />
                             </div>
 
-                            <ButtonRed
+                            <Button
                                 text="Remove"
+                                color="red"
                                 handleClick={() =>
                                     educationHandlers.remove(educationItem.key)
                                 }
@@ -367,8 +372,9 @@ const Form = () => {
                                 }
                             />
                         </div>
-                        <ButtonRed
+                        <Button
                             text="Remove"
+                            color="red"
                             handleClick={() =>
                                 skillsHandler.remove(skillItem.key)
                             }

commit 8f48e2b895769fc5437c666a65a8789866cee6ce

Fix problem with building class names dynamically
Problem was introduced in fd9d3c6

src/components/Button.jsx
@@ -4,12 +4,12 @@ export const Button = ({
     color = 'white',
     handleClick,
 }) => {
-    const bgColor = color !== 'white' ? `bg-${color}-500` : `bg-${color}`;
-    const textColor = color !== 'white' ? 'text-white' : 'text-black';
-    const hoverColor =
-        color !== 'white' ? `hover:bg-${color}-600` : 'hover:bg-white/80';
+    const colorVariants = {
+        red: 'bg-red-600 hover:bg-red-500 text-white',
+        white: 'bg-white hover:bg-white/80 text-black',
+    };
 
-    const classes = `w-full cursor-pointer ${textColor} rounded-sm ${bgColor} px-8 py-4 font-bold transition-colors ${hoverColor} active:scale-95`;
+    const classes = `w-full cursor-pointer rounded-sm ${colorVariants[color]} px-8 py-4 font-bold transition-colors active:scale-95`;
 
     return (
         <button type={type} onClick={handleClick} className={classes}>